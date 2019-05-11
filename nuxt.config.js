6cdf6d7ade61c22d4ba16d41a4bc86b2835adf8b
rriski <timo.h.riski@outlook.com>
2019-05-11 16:24:26 +0300

    Move global css to assets from layouts

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: ['~/assets/style/app.styl', '~/assets/_variables.scss'],
+  css: ['~/assets/style/app.styl', '~/assets/_variables.scss', '~/assets/_global.scss'],
 
   /*
    ** Plugins to load before mounting the App

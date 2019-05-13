6cc5d48330bbc32bc788b1046120526134409ebe
rriski <timo.h.riski@outlook.com>
2019-05-13 12:43:59 +0300

    Translate errors page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="container error__container">
-    <v-card class="elevation-3 pa-4">
+    <v-card v-if="lang === 'fi'" class="elevation-3 pa-4">
       <h1 v-if="error.statusCode === 404">Sivua ei löytynyt</h1>
       <h1 v-else>Tapahtui virhe</h1>
-      <nuxt-link to="/">Etusivulle</nuxt-link>
+      <nuxt-link to="/">Siirry etusivulle</nuxt-link>
+    </v-card>
+    <v-card v-else-if="lang === 'nl'" class="elevation-3 pa-4">
+      <h1 v-if="error.statusCode === 404">Pagina niet gevonden</h1>
+      <h1 v-else>Er is een fout opgetreden</h1>
+      <nuxt-link to="/">Ga naar de voorpagina</nuxt-link>
+    </v-card>
+    <v-card v-else-if="lang === 'en'" class="elevation-3 pa-4">
+      <h1 v-if="error.statusCode === 404">Page not found</h1>
+      <h1 v-else>An error occured</h1>
+      <nuxt-link to="/">Go to the front page</nuxt-link>
     </v-card>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['error']
+  props: ['error'],
+  computed: {
+    lang() {
+      return this.$store.state.language
+    }
+  }
 }
 </script>
 

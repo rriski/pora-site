0f0dbc752528d91212939a79cbd9244e0d0aacd6
rriski <timo.h.riski@outlook.com>
2019-05-18 01:31:17 +0300

    Fix error page links to front page

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,17 +3,17 @@
     <v-card v-if="lang === 'fi'" class="elevation-3 pa-4">
       <h1 v-if="error.statusCode === 404">Sivua ei löytynyt</h1>
       <h1 v-else>Tapahtui virhe</h1>
-      <nuxt-link to="/">Siirry etusivulle</nuxt-link>
+      <nuxt-link to="/fi/etusivu">Siirry etusivulle</nuxt-link>
     </v-card>
     <v-card v-else-if="lang === 'nl'" class="elevation-3 pa-4">
       <h1 v-if="error.statusCode === 404">Pagina niet gevonden</h1>
       <h1 v-else>Er is een fout opgetreden</h1>
-      <nuxt-link to="/">Ga naar de voorpagina</nuxt-link>
+      <nuxt-link to="/nl/etusivu">Ga naar de voorpagina</nuxt-link>
     </v-card>
     <v-card v-else-if="lang === 'en'" class="elevation-3 pa-4">
       <h1 v-if="error.statusCode === 404">Page not found</h1>
       <h1 v-else>An error occured</h1>
-      <nuxt-link to="/">Go to the front page</nuxt-link>
+      <nuxt-link to="/en/etusivu">Go to the front page</nuxt-link>
     </v-card>
   </div>
 </template>

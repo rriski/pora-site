1a3fb4a602dfef5aa94d875a688e3c286021753d
rriski <timo.h.riski@outlook.com>
2019-05-28 11:40:28 +0300

    Import only used flag icons
    Reduces bundle size

--- components/LanguagePicker.vue
+++ components/LanguagePicker.vue
@@ -12,7 +12,10 @@
       </template>
       <v-list>
         <v-list-tile>
-          <span class="flag flag-icon flag-icon-fi" />
+          <span
+            :style="{ 'background-image': 'url(' + fi + ')' }"
+            class="flag flag-icon flag-icon-fi"
+          />
           <v-list-tile-title>
             <nuxt-link :to="'/fi/etusivu'">
               Suomi
@@ -20,7 +23,10 @@
           </v-list-tile-title>
         </v-list-tile>
         <v-list-tile>
-          <span class="flag flag-icon flag-icon-nl" />
+          <span
+            :style="{ 'background-image': 'url(' + nl + ')' }"
+            class="flag flag-icon flag-icon-nl"
+          />
           <v-list-tile-title>
             <nuxt-link :to="'/nl/etusivu'">
               Netherlands
@@ -28,7 +34,10 @@
           </v-list-tile-title>
         </v-list-tile>
         <v-list-tile>
-          <span class="flag flag-icon flag-icon-gb" />
+          <span
+            :style="{ 'background-image': 'url(' + en + ')' }"
+            class="flag flag-icon"
+          />
           <v-list-tile-title>
             <nuxt-link :to="'/en/etusivu'">
               English
@@ -41,12 +50,40 @@
 </template>
 
 <script>
-import 'flag-icon-css/css/flag-icon.css'
-export default {}
+import fi from 'flag-icon-css/flags/4x3/fi.svg'
+import nl from 'flag-icon-css/flags/4x3/nl.svg'
+import en from 'flag-icon-css/flags/4x3/gb.svg'
+
+export default {
+  data() {
+    return {
+      fi: fi,
+      nl: nl,
+      en: en
+    }
+  }
+}
 </script>
 
 <style scoped lang="scss">
 .flag {
   margin-right: 5px;
+}
+
+.flag-icon-background {
+  background-size: contain;
+  background-position: 50%;
+  background-repeat: no-repeat;
+}
+
+.flag-icon {
+  @extend .flag-icon-background;
+  position: relative;
+  display: inline-block;
+  width: (4 / 3) * 1em;
+  line-height: 1em;
+  &:before {
+    content: '\00a0';
+  }
 }
 </style>
